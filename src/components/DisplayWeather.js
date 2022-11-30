3bae040b1feae49b58df635cddeb5c7fd2e8d61b
coolblast-coder <lyapunovantik@gmail.com>
2022-11-30 10:55:51 -0700

    created a card which displays city and country name

--- src/components/DisplayWeather.js
+++ src/components/DisplayWeather.js
@@ -1,10 +1,16 @@
 import React from 'react'
+import '../components/displayweather.css'
 
 function DisplayWeather(props) {
    console.log("props", props)
+    const {data} = props;
   return (
-    <div>
-        
+    <div className='displayWeather'>
+        <div className='maincard'>
+            <span className='cardtitle'>
+                {data.name}, {data.sys.country}. Weather
+            </span>
+        </div>
     </div>
   )
 }
