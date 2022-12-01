cc7adfc7be661296fa4af498eb9be1114d1122ba
coolblast-coder <lyapunovantik@gmail.com>
2022-12-01 11:17:44 -0700

    Finalized the table with various information. Also added error message in case user enters wrong city name

--- src/components/DisplayWeather.js
+++ src/components/DisplayWeather.js
@@ -4,9 +4,14 @@ import "./displayweather.css";
 function DisplayWeather(props) {
   console.log("props", props);
   const { data } = props;
+  const iconURL =
+    "http://openweathermap.org/img/wn/" + `${data.cod != 404 ? data.weather[0].icon : null}` + ".png";
   return (
+    
     <div className="displayweather">
-      <div className="maincard">
+      {data.cod != 404 ?
+      <React.Fragment>
+        <div className="maincard">
         <span className="cardtitle">
           {data.name}, {data.sys.country}. Weather
         </span>
@@ -14,7 +19,108 @@ function DisplayWeather(props) {
           As of {new Date().toLocaleTimeString()}
         </span>
         <h1>{Math.floor(data.main.temp - 273.15)}°</h1>
+        <span className="weather-main">{data.weather[0].main}</span>
+        <img src={iconURL} className="weather-icon" alt="" />
+        <span className="weather-description">
+          {data.weather[0].description}
+        </span>
+        
       </div>
+      <div className="weatherdetails">
+        <div className="section1">
+          <table>
+            <tr>
+              <td>
+                <h4>High/Low</h4>
+              </td>
+              <td>
+                <span>
+                  {Math.floor(data.main.temp_max - 273.15)}°/{" "}
+                  {Math.floor(data.main.temp_min - 273.15)}°{"C"}
+                </span>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <h4>Humidity</h4>
+              </td>
+              <td>
+                <span>
+                {data.main.humidity} %
+                </span>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <h4>Pressure</h4>
+              </td>
+              <td>
+                <span>
+                 {data.main.pressure} hPa
+                </span>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <h4>Visibility</h4>
+              </td>
+              <td>
+                <span>
+                {data.visibility/1000} Km
+                </span>
+              </td>
+            </tr>
+          </table>
+        </div>
+        <div className="section2">
+          <table>
+            <tr>
+              <td>
+                <h4>Wind speed</h4>
+              </td>
+              <td>
+                <span>
+                 {data.wind.speed} m/s
+                </span>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <h4>Wind direction</h4>
+              </td>
+              <td>
+                <span>
+                {data.wind.deg} °
+                </span>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <h4>Sunrise</h4>
+              </td>
+              <td>
+                <span>
+                 {new Date(data.sys.sunrise *1000).toLocaleTimeString()}
+                </span>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <h4>Sunset</h4>
+              </td>
+              <td>
+                <span>
+                 {new Date(data.sys.sunset *1000).toLocaleTimeString()}
+                </span>
+              </td>
+            </tr>
+          </table>
+        </div>
+      </div>
+      </React.Fragment>
+      : <div className="maincard">
+        <h2>{data.message}</h2></div>}
+   
     </div>
   );
 }
