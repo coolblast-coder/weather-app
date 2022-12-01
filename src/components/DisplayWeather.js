e6fde5f48a04bc8444554286d04401c4b15bb05b
coolblast-coder <lyapunovantik@gmail.com>
2022-12-01 09:14:55 -0700

    added card to display weather

--- src/components/DisplayWeather.js
+++ src/components/DisplayWeather.js
@@ -1,21 +1,22 @@
-import React from 'react'
-import './displayweather.css'
+import React from "react";
+import "./displayweather.css";
 
 function DisplayWeather(props) {
-   console.log("props", props)
-    const {data} = props;
+  console.log("props", props);
+  const { data } = props;
   return (
-    <div className='displayweather'>
-        <div className='maincard'>
-            <span className='cardtitle'>
-                {data.name}, {data.sys.country}. Weather
-            </span>
-            <span className='cardsubtitle'>
-                As of {new Date().toLocaleTimeString()}
-            </span>
-        </div>
+    <div className="displayweather">
+      <div className="maincard">
+        <span className="cardtitle">
+          {data.name}, {data.sys.country}. Weather
+        </span>
+        <span className="cardsubtitle">
+          As of {new Date().toLocaleTimeString()}
+        </span>
+        <h1>{Math.floor(data.main.temp - 273.15)}°</h1>
+      </div>
     </div>
-  )
-  }
+  );
+}
 
-export default DisplayWeather
+export default DisplayWeather;
